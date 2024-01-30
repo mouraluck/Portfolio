40830d1324016992ee5d7d47c3a6883093632989
O Moura <123185795+LucRevoltz207@users.noreply.github.com>
2024-01-30 12:52:48 -0300

    up 4

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let lastScrollTop = 0;
 const navbar = document.getElementById("navbar");
 const navbarHeight = navbar.clientHeight;
 
+
 window.addEventListener("scroll", function() {
   let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
   if (currentScroll > lastScrollTop) {
@@ -13,3 +14,24 @@ window.addEventListener("scroll", function() {
   }
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 }, false);
+
+function changeBackground() {
+  var checkbox = document.getElementById("dark");
+  var body = document.body;
+  
+  // Verifica o estado do checkbox
+  if (checkbox.checked) {
+    body.style.backgroundColor = "#e7e7e7"; // Altere para a cor desejada
+  } else {
+    body.style.backgroundColor = "#181818"; // Volta à cor original ou substitua pela cor desejada quando desmarcado
+  }
+}
+
+// Adiciona o evento onchange ao carregar a página
+document.addEventListener("DOMContentLoaded", function() {
+  // Chama a função para definir a cor de fundo inicial
+  changeBackground();
+  
+  // Adiciona o evento onchange ao checkbox
+  document.getElementById("dark").addEventListener("change", changeBackground);
+});
